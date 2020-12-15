3f4a8849356c57327e6717d60c7f198e3328c1ae
Santeri Hautala <71129078+Sunttu152@users.noreply.github.com>
2020-12-15 16:13:54 +0200

    updated login.js to work with user input

--- resources/user.py
+++ resources/user.py
@@ -6,6 +6,8 @@ from http import HTTPStatus
 from models.user import User
 from schemas.user import UserSchema
 
+# TODO UserListResource
+
 user_schema = UserSchema()
 user_list_schema = UserSchema(many=True)
 
@@ -24,18 +26,6 @@ class UserListResource(Resource):
 
         return user_list_schema.dump(users).data, HTTPStatus.OK
 
-    @jwt_required
-    def post(self):
-
-        json_data = request.get_json()
-
-        current_user = get_jwt_identity()
-
-        # Only for admins
-        if current_user is None:
-            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
-
-        data, errors = user_schema.load(data=json_data)
 
         if errors:
             return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
@@ -49,9 +39,9 @@ class UserListResource(Resource):
 class UserResource(Resource):
 
     @jwt_required
-    def get(self, user_id):
+    def get(self, username):
 
-        user = User.get_by_id(user_id=user_id)
+        user = User.get_by_username(username=username)
 
         current_user = get_jwt_identity()
 
@@ -89,4 +79,4 @@ class UserResource(Resource):
         user.updated_at = user.updated_at
         user.save()
 
-        return {'message': 'Updated'}, HTTPStatus.OK
+        return user_schema.dump(user).data, HTTPStatus.OK
--- templates/login.js
+++ templates/login.js
@@ -1,12 +1,29 @@
 function getToken(){
 
     
-    var x = document.getElementById("username").value;
-    var y = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+            
+// Sending and receiving data in JSON format using POST method
+//
+    if ( username == "Matti" && password == "testi"){
+        alert ("Login successfully");
+        window.location = "order.html"; // Redirecting to other page.
+
+        return false;
+    }
+        else{
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+        // Disabling fields after 3 attempts.
+        if( attempt == 0){
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("submit").disabled = true;
+        return false;
+        }
     
-    var x = {"username":"Matti"}
-
-    var y = {"password":"testi"}
-
-
+    
+    }
+ 
 };
